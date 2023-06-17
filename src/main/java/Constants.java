1751c4490e9d3a3c77ebddcdf904a842b77cccd2
janhaendl <jh@alphainside.eu>
2023-06-17 15:38:53 +0200

    constants Class added

---
+++ src/main/java/Constants.java
@@ -0,0 +1,15 @@
+public final class Constants {
+
+    private Constants(){
+        throw new AssertionError("Class for Constants only");
+    }
+    public enum MapType  {
+        FOOD,
+        HIVE,
+        NONE,
+        TRACE
+    }
+
+
+
+}
