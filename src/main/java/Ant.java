f5852744cf54cb53ccd69284c0832fe635088094
janhaendl <jh@alphainside.eu>
2023-06-17 15:37:29 +0200

    Ant triangulation for better rendering and separate class

--- src/main/java/Ant.java
+++ src/main/java/Ant.java
@@ -1,4 +1,11 @@
-public class Ant {
+import org.lwjgl.util.vector.Vector3f;
+
+public class Ant extends Entity{
+    public Ant(TexturedModel model, Vector3f position, float rotX, float rotY, float rotZ, float scale){
+        super(model,position,rotX,rotY,rotZ,scale);
+    }
+
+
 
 
 
